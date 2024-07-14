ca3af3883d88bd93224ec9d83142ddb5f290cd82
tejal khade <tejal.khade@intel.com>
2024-07-14 23:51:03 +0530

    fries

--- first.py
+++ first.py
@@ -3,6 +3,7 @@ x= input("are you hungry?")
 if x=="yes":
     print("Will eat samosa")
     print("will eat cake")
+    print("will eat fries")
 else:
     print("Back to work")
     print("Go to Play")
